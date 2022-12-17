c7f001a192b756da823faeae192f397491368f28
MSI\user <michal.potoczny.92@gmail.com>
2022-12-17 22:32:24 +0100

    Add specific in php kontak page

---
+++ page-kontakt.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package test
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+		<h1>KontaktPage</h1>
+	</main><!-- #main -->
+
+<?php
+get_footer();
